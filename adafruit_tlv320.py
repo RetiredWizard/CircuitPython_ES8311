cdbbf1ea7bf938dafd4d5f39f96ce56c49185dad
RetiredWizard <github@retiredwizard.com>
2025-09-06 22:21:42 -0400

    Add Beta CircuitPython firmware for RP2350pc

--- adafruit_tlv320.py
+++ adafruit_tlv320.py
@@ -1,7 +1,6 @@
 import circuitpython_es8311
 import board
 import digitalio
-import time
 
 print('**** Warning **** "FAKE" adafruit_tlv320.py library is being used to')
 print('allow applications written for the tlv320 device to run on the ESP8311 DAC')
@@ -17,7 +16,7 @@ shdn.direction=digitalio.Direction.OUTPUT
 shdn.value=False
 
 class TLV320DAC3100(circuitpython_es8311.ES8311):
-    
+
     def __init__(self, i2c: I2C, address: int = 0x18) -> None:
         super().__init__(i2c,address)
 
